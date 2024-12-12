4ce724d452e00aabd2a8ce35fc9086af3e953b31
Alonso <rcastillo20986@iesjoanramis.org>
2024-12-12 16:17:46 +0100

    creacion de tu mama

---
+++ src/holaMundo2/HolaMundo2.java
@@ -0,0 +1,28 @@
+package holaMundo2;
+
+import java.util.Scanner;
+
+public class HolaMundo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Ingresa tu nombre: ");
+		String nombre = scanner.nextLine();
+		System.out.println("¡Hola, " + nombre + "! Bienvenido a Java.");
+		
+		System.out.println("Solicita nuestro nombre para proceder a escribir un mensaje");
+		
+		System.out.println("No se que ocurre, ayuda");
+		
+		System.out.println("98DSFUI");
+		
+		System.out.println("tuputamadrearchivomaldito");
+		
+		System.out.println("dime que ahora si porfa");
+		
+		scanner.close();
+
+	}
+
+}
